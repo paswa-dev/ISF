06ec37299638c415386b25ce754bcfa93e9aa794
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2024-01-23 11:34:36 -0500

    Retard

--- ISF.lua
+++ ISF.lua
@@ -1,1 +1,10 @@
 --// Base
+--// Use CollectionService
+--[[
+.Loop (Allows tag function to be ran in a loop, requires a signal.)
+.Tag (Puts tag into tag table. Can be instanced, meaning it runs everytime in game instance tag is created)
+:Start()
+:BulkLoadTags(tags, functions)
+
+
+]]
